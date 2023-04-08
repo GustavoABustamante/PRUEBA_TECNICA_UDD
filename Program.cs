feda7fbe87d3da35c181ad3dfaa4e6a0b20ec3f2
Gustavo Bustamante <gustavobustamante1996@gmail.com>
2023-04-08 13:43:09 -0400

    Conexión entre API y SQL Server db en development network.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using PRUEBA_TECNICA_UDD.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+// 
+builder.Services.AddDbContext<DbContextClass>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("UddDb")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// **Solo para ambiente de desarrollo**
+// Se asegura de que la base de datos levantada en docker elimine los registros antiguos y luego se cree su schema
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<DbContextClass>();
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
